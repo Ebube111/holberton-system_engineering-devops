0226f6e38d47e10626d316b1547e4e37fa40769a
cedouiri <cedouiri@gmail.com>
2020-05-28 20:58:01 +0000

    fix puppet-lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,5 @@
 #under pressure and it turns out its not doing well: we are
 #getting a huge amount of failed requests
 exec { 'update_ulimit':
-	  command => 'sed -i \'s/15/15000/\' /etc/default/nginx && service nginx restart',
-	  path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
+      command => 'sed -i \'s/15/15000/\' /etc/default/nginx && service nginx restart',
+      path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
