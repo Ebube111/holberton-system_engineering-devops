b6170d04ca29ebf4ac3ad8269af73d2e6a98a31d
cedouiri <cedouiri@gmail.com>
2020-05-28 20:58:28 +0000

    user limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# testing how well our web server setup featuring Nginx is doing
+#under pressure and it turns out its not doing well: we are
+#getting a huge amount of failed requests
+exec { 'update_ulimit':
+      command => 'sed -i \'s/15/15000/\' /etc/default/nginx && service nginx restart',
+      path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
